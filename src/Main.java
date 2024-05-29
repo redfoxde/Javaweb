9db9fd2a1b6960e09b456cb2d452ea577abe52ef
陈鸿鑫 <fushiyuchen@outlook.com>
2024-05-29 22:23:27 +0800

    test

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,56 @@
-//TIP 要<b>运行</b>代码，请按 <shortcut actionId="Run"/> 或
-// 点击装订区域中的 <icon src="AllIcons.Actions.Execute"/> 图标。
-public class Main {
-    public static void main(String[] args) {
-        //TIP 当文本光标位于高亮显示的文本处时按 <shortcut actionId="ShowIntentionActions"/>
-        // 查看 IntelliJ IDEA 建议如何修正。
-        System.out.printf("Hello and welcome!");
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP 按 <shortcut actionId="Debug"/> 开始调试代码。我们已经设置了一个 <icon src="AllIcons.Debugger.Db_set_breakpoint"/> 断点
-            // 但您始终可以通过按 <shortcut actionId="ToggleLineBreakpoint"/> 添加更多断点。
-            System.out.println("i = " + i);
+public class Main {
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/hotelmanagementsystem", "root", "fushiyuchen");
+
+            try {
+                Statement statement = connection.createStatement();
+
+                try {
+                    int i = statement.executeUpdate("insert into rooms values(10122002,102,'标准间',120,0.86,'已预定')");
+                    System.out.println("生效了" + i + "行！");
+                } catch (Throwable var7) {
+                    if (statement != null) {
+                        try {
+                            statement.close();
+                        } catch (Throwable var6) {
+                            var7.addSuppressed(var6);
+                        }
+                    }
+
+                    throw var7;
+                }
+
+                if (statement != null) {
+                    statement.close();
+                }
+            } catch (Throwable var8) {
+                if (connection != null) {
+                    try {
+                        connection.close();
+                    } catch (Throwable var5) {
+                        var8.addSuppressed(var5);
+                    }
+                }
+
+                throw var8;
+            }
+
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (SQLException var9) {
+            SQLException ex = var9;
+            ex.printStackTrace();
         }
+
     }
 }
